46c517df24c4d91a7e8bd7888e8871cf0e789648
Melona0457 <boo4246@naver.com>
2026-05-18 16:19:57 +0900

    add: 배열, 이전, 이후 버튼 이벤트 추가

--- week02/JEONGIN/calendar/cal.js
+++ week02/JEONGIN/calendar/cal.js
@@ -1,7 +1,54 @@
 let date = new Date();
-let year = date.getFullYear();
-let month = date.getMonth();
-let firstday = new Date(year, month, 1).getDay();
+let currentYear = date.getFullYear();
+let currentMonth = date.getMonth();
+let prevBtn = document.getElementById("prev");
+let nextBtn = document.getElementById("next");
 
-document.getElementById("year").textContent = year;
-document.getElementById("month").textContent = month + 1 + "월";
+cal();
+
+
+function cal () {
+    let arr = [];
+    let i;
+
+    let firstDay = new Date(currentYear, currentMonth, 1).getDay();
+    let lastDay = new Date(currentYear, currentMonth+1, 0).getDate();
+
+    document.getElementById("year").textContent = currentYear;
+    document.getElementById("month").textContent = currentMonth + 1 + "월";
+
+    for (i = 0; i < firstDay; i++) {
+        arr[i] = "";
+    }
+
+    for (let j = 1; j <= lastDay; j++) {
+        arr[i] = j;
+        i++;
+    }
+
+    for (let k = 0; k < 42; k++) {
+        document.getElementById(k).textContent = arr[k];
+    }
+}
+
+prevBtn.addEventListener("click", () => {
+    currentMonth--;
+
+    if (currentMonth < 0) {
+        currentMonth = 11;
+        currentYear--;
+    }
+
+    cal();
+});
+
+nextBtn.addEventListener("click", () => {
+    currentMonth++;
+
+    if (currentMonth > 12) {
+        currentMonth = 0;
+        currentYear++;
+    }
+
+    cal();
+});
