3fb75fbc92fe0bad42baa6658546ffcc5ee8cc2f
parkseoyeon9163@gmail.com <162225988+o25e@users.noreply.github.com>
2026-05-18 11:59:12 +0900

    feat: 캘린더 주말에 색상 추가

--- week02/SEOYEON/01.calendar/script.js
+++ week02/SEOYEON/01.calendar/script.js
@@ -17,7 +17,7 @@ function drawCalendar(){
     // 이번 달 마지막 날짜
     let lastDate = new Date(year, month + 1, 0).getDate();
 
-    // 빈칸 만들기
+    // 앞 빈칸 만들기
     for(let i = 0; i < firstDay; i++){
         let empty = document.createElement("div");
         date.appendChild(empty);
@@ -30,10 +30,20 @@ function drawCalendar(){
         
         let week = new Date(year, month, i).getDay();
 
+        // 일요일
+        if(week == 0){
+            day.classList.add("red");
+        }
+
+        // 토요일
+        if(week == 6){
+            day.classList.add("blue");
+        }
+
         date.appendChild(day);
     }
 
-    // 뒤 빈칸 채우기
+    // 뒤 빈칸 만들기
     let total = firstDay + lastDate;
     let remain = 42 - total;
 
