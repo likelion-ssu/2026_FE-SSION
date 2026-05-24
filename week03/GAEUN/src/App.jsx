8c39b7ebf0bc58581c8d32836cf288cce2eac4c4
geuniey <geuniey@gmail.com>
2026-05-24 17:29:58 +0900

    chore: React 프로젝트 생성 및 Tailwind 설정

---
+++ week03/GAEUN/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-black text-3xl text-orange-500">
+      Instagram Feed Refactoring
+    </div>
+  );
+}
+
+export default App;
