edca03b33aa58254c91bf1e9b75022c02e58a2c7
parkseoyeon9163@gmail.com <162225988+o25e@users.noreply.github.com>
2026-05-26 00:46:24 +0900

    refactor: 사이드바 컴포넌트 분리

--- week03/SEOYEON/src/App.jsx
+++ week03/SEOYEON/src/App.jsx
@@ -1,60 +1,10 @@
 import "./App.css"
+import Sidebar from "./components/Sidebar";
 
 function App() {
     return (
         <div className="container">
-            {/* 왼쪽 사이드바 */}
-            <div id="left">
-                <div className="left-top">
-                    <i className="bi bi-instagram"></i>
-                </div>
-
-                <div className="left-center">
-                    <div className="navigation">
-                        <i className="bi bi-house-door-fill"></i>
-                        <span>홈</span>
-                    </div>
-                    <div className="navigation">
-                        <i className="bi bi-play-btn nav-icon"></i>
-                        <span>릴스</span>
-                    </div>
-                    <div className="navigation">
-                        <i className="bi bi-send"></i>
-                        <span>메시지</span>
-                    </div>
-                    <div className="navigation">
-                        <i className="bi bi-search nav-icon"></i>
-                        <span>검색</span>
-                    </div>
-                    <div className="navigation">
-                        <i className="bi bi-compass nav-icon"></i>
-                        <span>탐색 탭</span>
-                    </div>
-                    <div className="navigation">
-                        <i className="bi bi-heart nav-icon"></i>
-                        <span>알림</span>
-                    </div>
-                    <div className="navigation">
-                        <i className="bi bi-plus-lg"></i>
-                        <span>만들기</span>
-                    </div>
-                    <div className="navigation">
-                        <i className="bi bi-person-circle nav-icon"></i>
-                        <span>프로필</span>
-                    </div>
-                </div>
-
-                <div className="left-bottom">
-                    <div className="navigation">
-                        <i className="bi bi-list nav-icon"></i>
-                        <span>더 보기</span>
-                    </div>
-                    <div className="navigation">
-                        <i className="bi bi-grid"></i>
-                        <span>Meta의 다른 앱</span>
-                    </div>
-                </div>
-            </div>
+            <Sidebar></Sidebar>
 
             {/* 가운데 메인 피드 */}
             <div id="center">
