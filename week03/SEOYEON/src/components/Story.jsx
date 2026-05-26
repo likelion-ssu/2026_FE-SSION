d877579be9bccfad15b8063edfa5cd45f809807f
parkseoyeon9163@gmail.com <162225988+o25e@users.noreply.github.com>
2026-05-26 19:40:52 +0900

    refactor: 스토리 영역 Tailwind CSS 적용

--- week03/SEOYEON/src/components/Story.jsx
+++ week03/SEOYEON/src/components/Story.jsx
@@ -5,25 +5,25 @@ import globalmediaImg from "../assets/globalmedia.png";
 
 function Story() {
     return (
-        <div id="center-top">
-            <div className="center-top-detail">
-                <img src={profileImg} alt="profile" />
+        <div className="mb-[30px] flex gap-5 overflow-x-auto">
+            <div className="flex flex-col items-center gap-2 text-xs">
+                <img className="h-20 w-20 rounded-full border-[3.5px] border-[#ff0191]" src={profileImg} alt="profile" />
                 <span>seo_ye0n4</span>
             </div>
-            <div className="center-top-detail">
-                <img src={profileImg} alt="profile" />
+            <div className="flex flex-col items-center gap-2 text-xs">
+                <img className="h-20 w-20 rounded-full border-[3.5px] border-[#ff0191]" src={profileImg} alt="profile" />
                 <span>odooou4</span>
             </div>
-            <div className="center-top-detail">
-                <img src={leeunImg} alt="leeun" />
+            <div className="flex flex-col items-center gap-2 text-xs">
+                <img className="h-20 w-20 rounded-full border-[3.5px] border-[#ff0191]" src={leeunImg} alt="leeun" />
                 <span>leeun04</span>
             </div>
-            <div className="center-top-detail">
-                <img src={likelionImg} alt="likelion" />
+            <div className="flex flex-col items-center gap-2 text-xs">
+                <img className="h-20 w-20 rounded-full border-[3.5px] border-[#ff0191]" src={likelionImg} alt="likelion" />
                 <span>likelion_ssu</span>
             </div>
-            <div className="center-top-detail">
-                <img src={globalmediaImg} alt="globalmedia" />
+            <div className="flex flex-col items-center gap-2 text-xs">
+                <img className="h-20 w-20 rounded-full border-[3.5px] border-[#ff0191]" src={globalmediaImg} alt="globalmedia" />
                 <span>ssu_globalmedia</span>
             </div>
         </div>
