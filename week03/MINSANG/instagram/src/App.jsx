fc21d8e68566878be0fb34289e7cf6c15e137182
Min <ckalstkd1019@naver.com>
2026-05-26 14:30:50 +0900

    사이드바 제작완료

--- week03/MINSANG/instagram/src/App.jsx
+++ week03/MINSANG/instagram/src/App.jsx
@@ -1,46 +1,21 @@
-import SidebarItem from "./SidebarItem";
 
-import logo from "./assets/logo.png";
-import home from "./assets/home.png";
-import reels from "./assets/reels.png";
-import message from "./assets/message.png";
-import search from "./assets/search.png";
-import explore from "./assets/explore.png";
-import alram from "./assets/alram.png";
-import create from "./assets/create.png";
-import profile from "./assets/profile.png";
-import more from "./assets/more.png";
-import meta from "./assets/meta.png";
-
-
+import Sidebar from './Sidebar';
+import StoryProfile from './StoryProfile'
 
 export default function App() {
     return (
         <div className="bg-black text-white min-h-screen">
-          <aside className="fixed left-0 top-0 h-screen w-64 p-6 flex flex-col justify-between">
-            <div>
-              <SidebarItem icon={logo} text="" />
-            </div>
+          <Sidebar />
 
-            <div className="flex flex-col gap2">
-              <SidebarItem icon={home} text="홈" />
-              <SidebarItem icon={reels} text="릴스" />
-              <SidebarItem icon={message} text="메시지" />
-              <SidebarItem icon={search} text="검색" />
-              <SidebarItem icon={explore} text="탐색 탭" />
-              <SidebarItem icon={alram} text="알림" />
-              <SidebarItem icon={create} text="만들기" />
-              <SidebarItem icon={profile} text="프로필" />
-            </div>
-            
-            <div className="flex flex-col gap2">
-              <SidebarItem icon={more} text="더보기" />
-              <SidebarItem icon={meta} text="Meta의 다른 앱" />
-            </div>
-
-          </aside>
 
           <main>
+            <div className="flex flex-row justify-between">
+
+            </div>
+            
+            <div>
+
+            </div>
 
           </main>
 
